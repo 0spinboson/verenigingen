b34c43db75dfbd8a48733f4d88a2c03a17a2e2d1
Foppe <fjdh@disroot.org>
2025-04-17 19:41:42 +0200

    Update membership_type.py

--- verenigingen/verenigingen/doctype/membership_type/membership_type.py
+++ verenigingen/verenigingen/doctype/membership_type/membership_type.py
@@ -1,9 +1,18 @@
-# Copyright (c) 2025, Foppe de Haan and contributors
+# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
-class Membershiptype(Document):
-	pass
+class MembershipType(Document):
+	def validate(self):
+		if self.linked_item:
+			is_stock_item = frappe.db.get_value("Item", self.linked_item, "is_stock_item")
+			if is_stock_item:
+				frappe.throw(_("The Linked Item should be a service item"))
+
+def get_membership_type(razorpay_id):
+	return frappe.db.exists("Membership Type", {"razorpay_plan_id": razorpay_id})
