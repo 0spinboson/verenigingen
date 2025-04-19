e58c9581182ebb7fb781415eee8f74ddfd3466ee
Foppe <fjdh@disroot.org>
2025-04-19 23:54:19 +0200

    Update member.py

--- verenigingen/verenigingen/doctype/member/member.py
+++ verenigingen/verenigingen/doctype/member/member.py
@@ -15,7 +15,7 @@ from verenigingen.verenigingen.doctype.membership_type.membership_type import ge
 
 class Member(Document):
 	def before_save(self):
-		self.member_name = f'(self.first_name} {self.tussenvoegsel or ""} {self.last_name or ""}'
+		self.member_name = f'{self.first_name} {self.tussenvoegsel or ""} {self.last_name or ""}'
 		
 	def onload(self):
 		"""Load address and contacts in `__onload`"""
