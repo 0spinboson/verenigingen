0e098c0cabe0d1e21b3309340357d38adf9c3a1f
Foppe de Haan <fjdh@disroot.org>
2025-05-03 07:47:36 +0000

    add team report

---
+++ verenigingen/verenigingen/report/users_by_team.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2023, Your Name and contributors
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+    if not filters:
+        filters = {}
+        
+    columns = get_columns()
+    data = get_data(filters)
+    
+    return columns, data
+
+def get_columns():
+    return [
+        {
+            "label": "Team",
+            "fieldname": "team",
+            "fieldtype": "Link",
+            "options": "Team",
+            "width": 140
+        },
+        {
+            "label": "Team Lead",
+            "fieldname": "team_lead",
+            "fieldtype": "Link",
+            "options": "User",
+            "width": 140
+        },
+        {
+            "label": "User",
+            "fieldname": "user",
+            "fieldtype": "Link",
+            "options": "User",
+            "width": 140
+        },
+        {
+            "label": "User Name",
+            "fieldname": "user_full_name",
+            "fieldtype": "Data",
+            "width": 180
+        },
+        {
+            "label": "Role",
+            "fieldname": "role",
+            "fieldtype": "Data",
+            "width": 120
+        },
+        {
+            "label": "From Date",
+            "fieldname": "from_date",
+            "fieldtype": "Date",
+            "width": 100
+        },
+        {
+            "label": "To Date",
+            "fieldname": "to_date",
+            "fieldtype": "Date",
+            "width": 100
+        },
+        {
+            "label": "Active",
+            "fieldname": "is_active",
+            "fieldtype": "Check",
+            "width": 70
+        }
+    ]
+
+def get_data(filters):
+    conditions = []
+    values = {}
+    
+    if filters.get("team"):
+        conditions.append("team.name = %(team)s")
+        values["team"] = filters.get("team")
+        
+    if filters.get("user"):
+        conditions.append("tmr.user = %(user)s")
+        values["user"] = filters.get("user")
+        
+    if filters.get("active_only"):
+        conditions.append("tmr.is_active = 1")
+        
+    if filters.get("department"):
+        conditions.append("team.department = %(department)s")
+        values["department"] = filters.get("department")
+        
+    if filters.get("from_date"):
+        conditions.append("(tmr.to_date >= %(from_date)s OR tmr.to_date IS NULL)")
+        values["from_date"] = filters.get("from_date")
+        
+    if filters.get("to_date"):
+        conditions.append("tmr.from_date <= %(to_date)s")
+        values["to_date"] = filters.get("to_date")
+    
+    conditions_str = " AND ".join(conditions)
+    if conditions_str:
+        conditions_str = " AND " + conditions_str
+        
+    query = """
+        SELECT
+            team.name as team,
+            team.team_lead,
+            tmr.user,
+            CONCAT(usr.first_name, ' ', IFNULL(usr.last_name, '')) as user_full_name,
+            tmr.role,
+            tmr.from_date,
+            tmr.to_date,
+            tmr.is_active
+        FROM
+            `tabTeam` team
+        INNER JOIN
+            `tabTeam Member Role` tmr ON tmr.parent = team.name
+        LEFT JOIN
+            `tabUser` usr ON usr.name = tmr.user
+        WHERE
+            team.docstatus < 2
+            {conditions}
+        ORDER BY
+            team.name, tmr.user
+    """.format(conditions=conditions_str)
+    
+    return frappe.db.sql(query, values=values, as_dict=1)
