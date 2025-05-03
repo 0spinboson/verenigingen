22709c2cf25f813785275663568e9f0a70e98505
Foppe de Haan <fjdh@disroot.org>
2025-05-03 07:53:56 +0000

    rename

--- verenigingen/verenigingen/doctype/team/team.js
+++ verenigingen/verenigingen/doctype/team/team.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Foppe de Haan and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Teams", {
+// frappe.ui.form.on("Team", {
 // 	refresh(frm) {
 
 // 	},
--- verenigingen/verenigingen/doctype/team/team.py
+++ verenigingen/verenigingen/doctype/team/team.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Teams(Document):
+class Team(Document):
 	pass
