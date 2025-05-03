f8474789a298967b4769f1ce7a51027bfa59df62
Foppe de Haan <fjdh@disroot.org>
2025-05-03 07:39:37 +0000

    add teams

---
+++ verenigingen/verenigingen/doctype/team_member_role/team_member_role.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Foppe de Haan and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TeamMemberRole(Document):
+	pass
