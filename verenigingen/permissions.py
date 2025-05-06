2c9fedc73a239ef8d9516b2cbf2119cdd74b83cd
Foppe <fjdh@disroot.org>
2025-05-06 20:05:19 +0200

    Update permissions.py

--- verenigingen/permissions.py
+++ verenigingen/permissions.py
@@ -10,58 +10,14 @@ def get_member_permission_query(user):
     if "System Manager" in frappe.get_roles(user):
         return ""
         
-    # Membership Managers can access all members
-    if "Membership Manager" in frappe.get_roles(user):
+    # Membership Managers and Association Managers can access all members
+    if "Membership Manager" in frappe.get_roles(user) or "Association Manager" in frappe.get_roles(user) or "Verenigingen Manager" in frappe.get_roles(user):
         return ""
         
     # Membership Users can only view members
     if "Membership User" in frappe.get_roles(user):
         return ""
         
-    # Users can only see their own member record
-    return f"""(`tabMember`.`user` = '{user}')"""
-    
-def get_membership_permission_query(user):
-    """Permission query for Membership doctype"""
-    if not user:
-        user = frappe.session.user
-        
-    # System Managers can access all memberships
-    if "System Manager" in frappe.get_roles(user):
-        return ""
-        
-    # Membership Managers can access all memberships
-    if "Membership Manager" in frappe.get_roles(user):
-        return ""
-        
-    # Membership Users can only view memberships
-    if "Membership User" in frappe.get_roles(user):
-        return ""
-        
-    # Users can only see memberships linked to their member record
-    member = frappe.db.get_value("Member", {"user": user}, "name")
-    if member:
-        return f"""(`tabMembership`.`member` = '{member}')"""
-    else:
-        # If no member record is found, don't allow access to any memberships
-        return "1=0"
-def get_member_permission_query(user):
-    """Permission query for Member doctype"""
-    if not user:
-        user = frappe.session.user
-        
-    # System Managers can access all members
-    if "System Manager" in frappe.get_roles(user):
-        return ""
-        
-    # Membership Managers can access all members
-    if "Membership Manager" in frappe.get_roles(user):
-        return ""
-        
-    # Membership Users can only view members
-    if "Membership User" in frappe.get_roles(user):
-        return ""
-    
     # Board members can view chapter members
     member = frappe.db.get_value("Member", {"user": user}, "name")
     if member:
@@ -91,8 +47,8 @@ def get_membership_permission_query(user):
     if "System Manager" in frappe.get_roles(user):
         return ""
         
-    # Membership Managers can access all memberships
-    if "Membership Manager" in frappe.get_roles(user):
+    # Membership Managers and Association Managers can access all memberships
+    if "Membership Manager" in frappe.get_roles(user) or "Association Manager" in frappe.get_roles(user) or "Verenigingen Manager" in frappe.get_roles(user):
         return ""
         
     # Membership Users can only view memberships
