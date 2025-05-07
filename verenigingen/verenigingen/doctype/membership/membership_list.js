82ccb054049a1c33618811cd74e09b5440d572a4
Foppe <fjdh@disroot.org>
2025-05-07 23:53:39 +0200

    Update membership_list.js

--- verenigingen/verenigingen/doctype/membership/membership_list.js
+++ verenigingen/verenigingen/doctype/membership/membership_list.js
@@ -1,12 +1,12 @@
 frappe.listview_settings['Membership'] = {
 	get_indicator: function(doc) {
-		if (doc.membership_status == 'New') {
+		if (doc.status == 'New') {
 			return [__('New'), 'blue', 'membership_status,=,New'];
-		} else if (doc.membership_status === 'Current') {
+		} else if (doc.status === 'Current') {
 			return [__('Current'), 'green', 'membership_status,=,Current'];
-		} else if (doc.membership_status === 'Pending') {
+		} else if (doc.status === 'Pending') {
 			return [__('Pending'), 'yellow', 'membership_status,=,Pending'];
-		} else if (doc.membership_status === 'Expired') {
+		} else if (doc.status === 'Expired') {
 			return [__('Expired'), 'grey', 'membership_status,=,Expired'];
 		} else {
 			return [__('Cancelled'), 'red', 'membership_status,=,Cancelled'];
