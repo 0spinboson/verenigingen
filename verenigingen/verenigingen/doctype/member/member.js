88b8363089d2e5cd35b8a55787c6b0a5f3db94b7
Foppe <fjdh@disroot.org>
2025-05-10 09:37:53 +0200

    Update member.js

--- verenigingen/verenigingen/doctype/member/member.js
+++ verenigingen/verenigingen/doctype/member/member.js
@@ -14,6 +14,7 @@ frappe.ui.form.on('Member', {
                 });
             }, __('Actions'));
         }
+        
         // In the refresh function in member.js
         if (frm.doc.customer) {
             // Your existing buttons that require customer to be set
@@ -54,7 +55,7 @@ frappe.ui.form.on('Member', {
             },
             callback: function(r) {
                 if (r.message && r.message.length) {
-                // Show board memberships
+                    // Show board memberships
                     var html = '<div class="board-memberships"><h4>Board Positions</h4><ul>';
 
                     r.message.forEach(function(board) {
@@ -83,44 +84,44 @@ frappe.ui.form.on('Member', {
                 });
             }, __('Actions'));
         }
-	
+        
         frm.add_custom_button(__('Create Volunteer'), function() {
-        // First get the email domain from settings
-        frappe.call({
-            method: 'frappe.client.get_value',
-            args: {
-                doctype: 'Verenigingen Settings',
-                fieldname: 'organization_email_domain'
-            },
-            callback: function(r) {
-                // Default domain if not set
-                const domain = r.message && r.message.organization_email_domain 
-                    ? r.message.organization_email_domain 
-                    : 'example.org';
-            
-                // Generate organization email based on full name
-                // Replace spaces with dots and convert to lowercase
-                const nameForEmail = frm.doc.full_name 
-                    ? frm.doc.full_name.replace(/\s+/g, '.').toLowerCase()
-                    : '';
-            
-                // Construct organization email
-                const orgEmail = nameForEmail ? `${nameForEmail}@${domain}` : '';
-            
-                // Set route options for creating volunteer
-                frappe.route_options = {
-                    'volunteer_name': frm.doc.full_name,
-                    'member': frm.doc.name,
-                    'preferred_pronouns': frm.doc.pronouns,
-                    'email': orgEmail,  // Organization email
-                     'personal_email': frm.doc.email || ''  // Personal email from member
-                };
-            
-                // Create new volunteer doc
-                frappe.new_doc('Volunteer');
-            }
-        });
-    }, __('Actions'));
+            // First get the email domain from settings
+            frappe.call({
+                method: 'frappe.client.get_value',
+                args: {
+                    doctype: 'Verenigingen Settings',
+                    fieldname: 'organization_email_domain'
+                },
+                callback: function(r) {
+                    // Default domain if not set
+                    const domain = r.message && r.message.organization_email_domain 
+                        ? r.message.organization_email_domain 
+                        : 'example.org';
+                
+                    // Generate organization email based on full name
+                    // Replace spaces with dots and convert to lowercase
+                    const nameForEmail = frm.doc.full_name 
+                        ? frm.doc.full_name.replace(/\s+/g, '.').toLowerCase()
+                        : '';
+                
+                    // Construct organization email
+                    const orgEmail = nameForEmail ? `${nameForEmail}@${domain}` : '';
+                
+                    // Set route options for creating volunteer
+                    frappe.route_options = {
+                        'volunteer_name': frm.doc.full_name,
+                        'member': frm.doc.name,
+                        'preferred_pronouns': frm.doc.pronouns,
+                        'email': orgEmail,  // Organization email
+                        'personal_email': frm.doc.email || ''  // Personal email from member
+                    };
+                
+                    // Create new volunteer doc
+                    frappe.new_doc('Volunteer');
+                }
+            });
+        }, __('Actions'));
         
         // Add button to create a new membership
         frm.add_custom_button(__('Create Membership'), function() {
@@ -179,11 +180,81 @@ frappe.ui.form.on('Member', {
                 }
             });
         }, __('View'));
+        
+        // SEPA Mandate Management Buttons
+        if (!frm.is_new()) {
+            // Add button to create new SEPA mandate
+            frm.add_custom_button(__('Create SEPA Mandate'), function() {
+                frappe.call({
+                    method: 'create_sepa_mandate',
+                    doc: frm.doc,
+                    callback: function(r) {
+                        if (r.message) {
+                            frappe.set_route('Form', 'SEPA Mandate', r.message);
+                        }
+                    }
+                });
+            }, __('Actions'));
+            
+            // Add button to view SEPA mandates
+            frm.add_custom_button(__('View SEPA Mandates'), function() {
+                frappe.set_route('List', 'SEPA Mandate', {
+                    'member': frm.doc.name
+                });
+            }, __('View'));
+            
+            // If there's a default mandate, add button to view it
+            if (frm.doc.default_sepa_mandate) {
+                frm.add_custom_button(__('Default SEPA Mandate'), function() {
+                    frappe.set_route('Form', 'SEPA Mandate', frm.doc.default_sepa_mandate);
+                }, __('View'));
+            }
+            
+            // Add custom button for SEPA mandates grid
+            if (frm.fields_dict['sepa_mandates']) {
+                frm.fields_dict['sepa_mandates'].grid.add_custom_button(__('Create New Mandate'), 
+                    function() {
+                        frappe.new_doc('SEPA Mandate', {
+                            'member': frm.doc.name,
+                            'member_name': frm.doc.full_name,
+                            'account_holder_name': frm.doc.full_name
+                        });
+                    }
+                );
+            }
+        }
 
         // Attach triggers to name fields dynamically
         ['first_name', 'middle_name', 'last_name'].forEach(field => {
             frm.fields_dict[field].df.onchange = () => frm.trigger('update_full_name');
         });
+    },
+    
+    onload: function(frm) {
+        if (!frm.is_new()) {
+            // Check if member has active SEPA mandates
+            frappe.call({
+                method: 'vereiningen.verenigingen.doctype.member.member.check_sepa_mandate_status',
+                args: {
+                    member: frm.doc.name
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        if (!r.message.has_active_mandate) {
+                            frm.dashboard.add_indicator(
+                                __('No Active SEPA Mandate'), 
+                                'orange'
+                            );
+                        } else if (r.message.expiring_soon) {
+                            frm.dashboard.add_indicator(
+                                __('SEPA Mandate Expiring Soon'), 
+                                'yellow'
+                            );
+                        }
+                    }
+                }
+            });
+        }
     },
     
     update_full_name: function(frm) {
@@ -195,6 +266,69 @@ frappe.ui.form.on('Member', {
         ].filter(Boolean).join(' ');
         
         frm.set_value('full_name', full_name);
+    },
+    
+    default_sepa_mandate: function(frm) {
+        // When default mandate is changed, update the table
+        if (frm.doc.default_sepa_mandate) {
+            let found = false;
+            
+            frm.doc.sepa_mandates.forEach(function(mandate) {
+                if (mandate.sepa_mandate === frm.doc.default_sepa_mandate) {
+                    frappe.model.set_value(mandate.doctype, mandate.name, 'is_default', 1);
+                    found = true;
+                } else if (mandate.is_default) {
+                    frappe.model.set_value(mandate.doctype, mandate.name, 'is_default', 0);
+                }
+            });
+            
+            // If the selected default mandate is not in the table, add it
+            if (!found && frm.doc.default_sepa_mandate) {
+                const new_row = frm.add_child('sepa_mandates');
+                new_row.sepa_mandate = frm.doc.default_sepa_mandate;
+                new_row.is_default = 1;
+                frm.refresh_field('sepa_mandates');
+            }
+        }
+    }
+});
+
+// Handlers for the Member SEPA Mandate Link child table
+frappe.ui.form.on('Member SEPA Mandate Link', {
+    sepa_mandate: function(frm, cdt, cdn) {
+        // When a mandate is selected, fetch its details
+        const row = locals[cdt][cdn];
+        
+        if (row.sepa_mandate) {
+            frappe.db.get_value('SEPA Mandate', row.sepa_mandate, 
+                ['mandate_id', 'status', 'sign_date', 'expiry_date'], 
+                function(r) {
+                    if (r) {
+                        frappe.model.set_value(cdt, cdn, 'mandate_reference', r.mandate_id);
+                        frappe.model.set_value(cdt, cdn, 'status', r.status);
+                        frappe.model.set_value(cdt, cdn, 'valid_from', r.sign_date);
+                        frappe.model.set_value(cdt, cdn, 'valid_until', r.expiry_date);
+                    }
+                }
+            );
+        }
+    },
+    
+    is_default: function(frm, cdt, cdn) {
+        // When setting a mandate as default, unset others
+        const row = locals[cdt][cdn];
+        
+        if (row.is_default) {
+            // Update parent's default_sepa_mandate field
+            frm.set_value('default_sepa_mandate', row.sepa_mandate);
+            
+            // Unset default on other mandates
+            frm.doc.sepa_mandates.forEach(function(mandate) {
+                if (mandate.name !== cdn && mandate.is_default) {
+                    frappe.model.set_value(mandate.doctype, mandate.name, 'is_default', 0);
+                }
+            });
+        }
     }
 });
 
