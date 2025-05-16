d6c1a1883140be9c91b2e873a705dab8780d4b64
Foppe <fjdh@disroot.org>
2025-05-17 00:18:48 +0200

    Update test_member.py

--- verenigingen/verenigingen/doctype/member/test_member.py
+++ verenigingen/verenigingen/doctype/member/test_member.py
@@ -1,9 +1,29 @@
-# Copyright (c) 2025, Foppe de Haan and Contributors
-# See license.txt
+# vereinigingen/vereinigingen/doctype/member/test_member.py
 
-# import frappe
 from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.utils import add_days, today
 
-
-class TestMember(FrappeTestCase):
-	pass
+class TestMemberFields(FrappeTestCase):
+    def test_field_existence(self):
+        """Test that fields referenced in code actually exist in the DocType"""
+        # Get DocType metadata
+        member_meta = frappe.get_meta("Member")
+        
+        # List of fields referenced in Member methods
+        critical_fields = [
+            "first_name", "middle_name", "last_name", "full_name", 
+            "email", "mobile_no", "customer", "user", "payment_method",
+            "iban", "bank_account_name", "primary_chapter"
+        ]
+        
+        # Check each field exists
+        missing_fields = []
+        for field in critical_fields:
+            if not member_meta.has_field(field):
+                missing_fields.append(field)
+        
+        self.assertEqual(
+            len(missing_fields), 0, 
+            f"The following fields are referenced in code but don't exist in Member DocType: {', '.join(missing_fields)}"
+        )
