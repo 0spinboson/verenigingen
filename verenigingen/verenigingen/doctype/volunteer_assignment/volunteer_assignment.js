730a92a714ac55555a31cefe3cde3464ddd5416a
Foppe <fjdh@disroot.org>
2025-05-17 23:01:03 +0200

    Update volunteer_assignment.js

--- verenigingen/verenigingen/doctype/volunteer_assignment/volunteer_assignment.js
+++ verenigingen/verenigingen/doctype/volunteer_assignment/volunteer_assignment.js
@@ -2,11 +2,41 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Volunteer Assignment', {
-    // Since this is a child table DocType, we're adding more functionality
-    // directly in the parent form's JS file (volunteer.js)
+    // When assignment type changes, set reference_doctype based on assignment type
+    assignment_type: function(frm, cdt, cdn) {
+        var child = locals[cdt][cdn];
+        
+        // Set reference doctype based on assignment type
+        if(child.assignment_type === 'Board Position') {
+            frappe.model.set_value(cdt, cdn, 'reference_doctype', 'Chapter');
+        } else if(child.assignment_type === 'Team') {
+            frappe.model.set_value(cdt, cdn, 'reference_doctype', 'Volunteer Team');
+        } else if(child.assignment_type === 'Committee') {
+            frappe.model.set_value(cdt, cdn, 'reference_doctype', 'Volunteer Team');
+        } else if(child.assignment_type === 'Event') {
+            frappe.model.set_value(cdt, cdn, 'reference_doctype', 'Event');
+        } else if(child.assignment_type === 'Commission') {
+            frappe.model.set_value(cdt, cdn, 'reference_doctype', 'Commission');
+        }
+        
+        // Refresh the parent form to update UI and apply filters
+        frm.refresh_field('active_assignments');
+    },
     
+    // When reference doctype changes, clear the reference name
     reference_doctype: function(frm, cdt, cdn) {
-        // When reference doctype changes, clear the reference name
         frappe.model.set_value(cdt, cdn, 'reference_name', '');
+        
+        // Refresh the parent form to update UI and apply filters
+        frm.refresh_field('active_assignments');
+        
+        // Force a re-query of the reference_name field to apply the latest filters
+        var child = locals[cdt][cdn];
+        if(child.reference_doctype) {
+            setTimeout(function() {
+                // This forces the dynamic link to refresh its options
+                frm.fields_dict.active_assignments.grid.grid_rows_by_docname[cdn].refresh_field('reference_name');
+            }, 300);
+        }
     }
 });
