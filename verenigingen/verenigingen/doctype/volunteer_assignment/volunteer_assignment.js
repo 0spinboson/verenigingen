86027e8475e4c0df821579c608dfeb33d8409899
Foppe <fjdh@disroot.org>
2025-05-17 22:18:44 +0200

    Update volunteer_assignment.js

--- verenigingen/verenigingen/doctype/volunteer_assignment/volunteer_assignment.js
+++ verenigingen/verenigingen/doctype/volunteer_assignment/volunteer_assignment.js
@@ -2,6 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Volunteer Assignment', {
+    // Since this is a child table DocType, we're adding more functionality
+    // directly in the parent form's JS file (volunteer.js)
+    
     reference_doctype: function(frm, cdt, cdn) {
         // When reference doctype changes, clear the reference name
         frappe.model.set_value(cdt, cdn, 'reference_name', '');
