55571c0408b5b3f228e12e36690641a0fb8963c1
Foppe <fjdh@disroot.org>
2025-05-17 23:11:42 +0200

    Update volunteer_team.py

--- verenigingen/verenigingen/doctype/volunteer_team/volunteer_team.py
+++ verenigingen/verenigingen/doctype/volunteer_team/volunteer_team.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe import _  # Add this import
+from frappe import _
 from frappe.model.document import Document
 
 class VolunteerTeam(Document):
