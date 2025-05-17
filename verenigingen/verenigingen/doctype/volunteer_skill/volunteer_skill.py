060aaa818e7ba1e91268649d226e5c380468defe
Foppe <fjdh@disroot.org>
2025-05-17 19:58:41 +0200

    Update volunteer_skill.py

--- verenigingen/verenigingen/doctype/volunteer_skill/volunteer_skill.py
+++ verenigingen/verenigingen/doctype/volunteer_skill/volunteer_skill.py
@@ -1,9 +1,16 @@
-# Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2025, Your Organization and contributors
 # For license information, please see license.txt
-
 
 from frappe.model.document import Document
 
-
 class VolunteerSkill(Document):
-	pass
+    def validate(self):
+        # Ensure proficiency level is valid
+        if hasattr(self, 'proficiency_level') and self.proficiency_level:
+            level = self.proficiency_level.split(' - ')[0] if ' - ' in self.proficiency_level else self.proficiency_level
+            try:
+                level_value = int(level)
+                if level_value < 1 or level_value > 5:
+                    frappe.throw(_("Proficiency level must be between 1 and 5"))
+            except ValueError:
+                frappe.throw(_("Invalid proficiency level format"))
