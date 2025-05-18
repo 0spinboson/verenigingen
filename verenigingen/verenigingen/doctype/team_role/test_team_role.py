028af2283f4275ffc0d10519d31374ac06cd2b00
Foppe <fjdh@disroot.org>
2025-05-18 13:52:47 +0200

    Delete verenigingen/verenigingen/doctype/team_role directory

--- verenigingen/verenigingen/doctype/team_role/test_team_role.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Foppe de Haan and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTeamRole(FrappeTestCase):
-	pass
