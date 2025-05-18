7ee268ef21397dc987956cfae81d1e9bc7ab2adb
Foppe <fjdh@disroot.org>
2025-05-18 13:51:12 +0200

    Delete verenigingen/verenigingen/doctype/volunteer_team_member directory

--- verenigingen/verenigingen/doctype/volunteer_team_member/volunteer_team_member.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2025, Your Organization and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-class VolunteerTeamMember(Document):
-    pass
