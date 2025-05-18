a4cccac51bc19e9f0f487610f34360ed3e3c053a
Foppe <fjdh@disroot.org>
2025-05-18 13:45:47 +0200

    Update and rename volunteer_team_responsibility.js to team_responsibility.js

--- verenigingen/verenigingen/doctype/team_responsibility/team_responsibility.js
+++ verenigingen/verenigingen/doctype/team_responsibility/team_responsibility.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2025, Your Organization and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Volunteer Team Responsibility', {
+frappe.ui.form.on('Team Responsibility', {
     // Nothing needed here since this is a child table
 });
